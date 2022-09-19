2af598903a8fe1880d4cda0f5de0aaa1b2f5df99
tanakanotarou2 <sotaime+gh@gmail.com>
2022-09-19 18:53:58 +0900

    複数メッセージを表示するように

--- frontend/src/components/Index/LoginBox/index.tsx
+++ frontend/src/components/Index/LoginBox/index.tsx
@@ -1,46 +1,114 @@
-import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader, Container,
-  Paper,
-  Stack,
-  TextField,
-  Typography,
-} from '@mui/material';
+import { Box, Button, Container, Paper, Stack, TextField, Typography } from '@mui/material';
+import { apiClient } from '@/lib/apiClient';
+import { Controller, useForm } from 'react-hook-form';
 
-import { useTheme } from '@mui/material/styles';
+import { useMutation } from 'react-query';
+import { useState } from 'react';
+import { TokenObtainPairRequest } from '../../../api/@types';
+import { AxiosError } from 'axios';
+import FieldErrorMessages from '@/components/shared/FieldErrorMessages';
+import { useAtom } from 'jotai';
+import { messageAtom } from '@/lib/jotaiAtom';
 
 const LoginBox = () => {
-  // const theme = useTheme();
-  return (
+  const [loading, setLoading] = useState(false);
+  const [, addMessage] = useAtom(messageAtom);
 
+  const {
+    handleSubmit,
+    control,
+    setError,
+    formState: { errors },
+  } = useForm<TokenObtainPairRequest>();
+
+  function postLogin(body: TokenObtainPairRequest) {
+    return apiClient.auth.token.$post({ body });
+  }
+
+  const mutation = useMutation(postLogin, {
+    onSuccess: () => {
+      // TODO: ログイン後ページへ遷移
+      addMessage({ text: 'ログインしました', 'variant': 'success' });
+    },
+    onError: (error: AxiosError) => {
+      console.log('err', error);
+      addMessage({ text: '不明なエラー', 'variant': 'error' });
+      // const err: any = apiErrorHandler.putError(error);
+      // if (err instanceof SingleErrorMessage) {
+      //   addMessage({ text: err.message, 'variant': 'warning' });
+      // } else if (err instanceof FormErrors) {
+      //   setFormErrors(setError, err.errors);
+      // } else {
+      //   console.log('err', error);
+      //   addMessage({ text: '不明なエラー', 'variant': 'error' });
+      // }
+    },
+    onSettled: () => {
+      setLoading(false);
+    },
+
+  });
+
+  const onSubmit = (data: TokenObtainPairRequest) => {
+    setLoading(true);
+    mutation.mutate(data);
+  };
+  return (
     <Container maxWidth='sm'>
       <Paper>
         <Box sx={{ p: 3 }}>
           <Typography component='h2' variant='h5' sx={{ mb: 2, textAlign: 'center' }}>ログイン</Typography>
 
-          <Stack spacing={3}>
-            <TextField
-              id='username'
-              sx={{ widht: '100%' }}
-            ></TextField>
-            <TextField
-              id='filled-password-input'
-              label='Password'
-              type='password'
-              sx={{ widht: '100%' }}
-              autoComplete='current-password'
-              variant='filled'
-            />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={2}>
+              <Controller
+                name='username'
+                control={control}
+                defaultValue=''
+                rules={{ required: '入力してください' }}
 
-            <Button
-              variant='contained'
-              sx={{ 'width': '100%', mx: 1 }}
-            >Login</Button>
-          </Stack>
+                render={({ field }) =>
+                  <TextField {...field}
+                             label='ユーザー名'
+                             required
+                             InputLabelProps={{
+                               shrink: true,
+                             }}
+                             error={!!errors.username}
+                             helperText={errors.username?.message}
+                  />}
+              />
+              <FieldErrorMessages name='username' errors={errors} />
+
+              <Controller
+                name='password'
+                control={control}
+                rules={{ required: '入力してください' }}
+                defaultValue=''
+                render={({ field }) =>
+                  <TextField {...field}
+                             label='パスワード'
+                             required
+                             type='password'
+                             variant='filled'
+                             InputLabelProps={{
+                               shrink: true,
+                             }}
+                    error={!!errors.password}
+                    helperText={errors.password?.message}
+                  />
+                }
+              />
+              <FieldErrorMessages name='password' errors={errors} />
+
+              <Button
+                type='submit'
+                variant='contained'
+                disabled={loading}
+              >Login</Button>
+            </Stack>
+          </form>
+
         </Box>
       </Paper>
     </Container>
