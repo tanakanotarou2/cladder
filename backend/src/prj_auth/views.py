e92634097c740ce2950b1fe9204ea1feeb0442e6
tanakanotarou2 <sotaime+gh@gmail.com>
2022-09-21 16:43:00 +0900

    CSRFView の整理

--- backend/src/prj_auth/views.py
+++ backend/src/prj_auth/views.py
@@ -1,21 +1,22 @@
-from django.http import JsonResponse
 from django.middleware.csrf import get_token
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_cookie
-from rest_framework.decorators import api_view
+from drf_spectacular.utils import extend_schema, extend_schema_view, inline_serializer
+from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
+@extend_schema_view(
+    get=extend_schema(responses={200: inline_serializer(name='CSRFResponse',
+                                                        fields={'csrfToken': serializers.CharField()}
+                                                        )},
+                      description="CSRFトークンを取得します"),
+)
 class CSRFView(APIView):
     # クッキーごとにキャッシュするのはありかもしれない
-    @method_decorator(cache_page(30))
+    @method_decorator(cache_page(10))
     @method_decorator(vary_on_cookie)
     def get(self, request, format=None):
         return Response({'csrfToken': get_token(request)})
-
-
-@api_view()
-def ping(request):
-    return JsonResponse({'result': 'OK'})
