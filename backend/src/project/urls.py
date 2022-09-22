24385a91a834e3132ec36903969403c10300a949
tanakanotarou2 <sotaime+gh@gmail.com>
2022-09-22 22:33:09 +0900

    smallfix

--- backend/src/project/urls.py
+++ backend/src/project/urls.py
@@ -11,16 +11,3 @@ urlpatterns = [
 urlpatterns += [
     path("api/", include(api_urls)),
 ]
-
-# DJOser の一部のエンドポイントだけを使用します。
-user_router = routers.SimpleRouter()
-user_router.register("users", DJOserUserViewSet)
-
-# 使用するエンドポイントの URL name
-enable_url_names = (
-    "user-set-password",
-    "user-list",
-)
-auth_patterns = [pat for pat in user_router.urls if pat.name in enable_url_names]
-
-urlpatterns += [path("api/auth/", include(auth_patterns)), ]
