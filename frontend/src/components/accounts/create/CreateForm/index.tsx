ce40a1d6ae41a1e59735572f56b3c7f06ee09f69
tanakanotarou2 <sotaime+gh@gmail.com>
2022-11-28 09:12:59 +0900

    :sparkles: 登録後にユーザー情報をコピーできるようにした

--- frontend/src/components/accounts/create/CreateForm/index.tsx
+++ frontend/src/components/accounts/create/CreateForm/index.tsx
@@ -1,17 +1,29 @@
-import { Box, Button, Checkbox, FormControlLabel, Stack, TextField } from '@mui/material';
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  IconButton,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
-
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import FieldErrorMessages from '@/components/shared/FieldErrorMessages';
 import { useAtom } from 'jotai';
 import { messageAtom } from '@/lib/jotaiAtom';
 import FormErrorMessages from '@/components/shared/FormErrorMessages';
-import { RegisterUserRequest, UserDetail } from '@/api/@types';
+import { RegisterUserRequest } from '@/api/@types';
 import { preprocessApiError, reformatToHookFormStyle } from '@/lib/apiErrorHandle';
 import { useMutation } from 'react-query';
 import { apiClient } from '@/lib/apiClient';
 import { AxiosError } from 'axios';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 /*
  * ランダムなパスワードを作成
@@ -25,10 +37,10 @@ const generatePassword = (): string => {
 
 const CreateForm = () => {
   const [loading, setLoading] = useState(false);
+  const [registeredUserAuthInfo, setRegisteredUserAuthInfo] = useState<string | null>(null);
   const [enabledRandomPassword, setEnabledRandomPassword] = useState(false);
   const [, addMessage] = useAtom(messageAtom);
   const [nonFieldErrors, setNonFieldErrors] = useState<string[] | null>(null);
-  const router = useRouter();
   const {
     handleSubmit,
     control,
@@ -41,8 +53,14 @@ const CreateForm = () => {
   };
 
   const mutation = useMutation(postRegister, {
-    onSuccess: (res) => {
-      addMessage({ text: `登録しました`, 'variant': 'success' });
+    onSuccess: (res, variables) => {
+      if (enabledRandomPassword) {
+        setRegisteredUserAuthInfo(
+          `ユーザー名: ${variables['username']}, パスワード: ${variables['password']}`);
+      } else {
+        setRegisteredUserAuthInfo(
+          `ユーザー名: ${variables['username']}`);
+      }
       console.log('res', res);
     },
     onError: (error: AxiosError) => {
@@ -70,122 +88,143 @@ const CreateForm = () => {
     mutation.mutate(data);
     setLoading(true);
     setNonFieldErrors(null);
+    setRegisteredUserAuthInfo(null);
   };
 
   const handleChangeRandomPassword = () => {
     setEnabledRandomPassword(!enabledRandomPassword);
   };
-  console.log('a', errors.username);
+
+  const registeredMessage = () => {
+    if (!registeredUserAuthInfo) return (<></>);
+    return (
+      <Box my={2}>
+        <Alert severity='success'
+               action={
+                 <CopyToClipboard text={registeredUserAuthInfo}
+                                  onCopy={() => addMessage('ユーザー情報をコピーしました')}>
+                   <IconButton>
+                     <ContentCopyIcon />
+                   </IconButton>
+                 </CopyToClipboard>
+               }
+        >
+          <AlertTitle>登録しました</AlertTitle>
+          <Typography variant='subtitle2' component='span'>{registeredUserAuthInfo}</Typography>
+        </Alert>
+      </Box>
+    );
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Stack spacing={2}>
-        <Controller
-          name='username'
-          control={control}
-          defaultValue=''
-          rules={{ required: '入力してください', minLength: { value: 4, message: '4文字以上入力してください' } }}
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={2}>
+          <Controller
+            name='username'
+            control={control}
+            defaultValue=''
+            rules={{ required: '入力してください', minLength: { value: 4, message: '4文字以上入力してください' } }}
 
-          render={({ field }) =>
-            <TextField {...field}
-                       label='ユーザー名'
-                       required
-                       InputLabelProps={{
-                         shrink: true,
-                       }}
-                       error={!!errors.username}
-                       helperText={errors.username?.message || '英数字で4文字以上入力してください'}
-            />}
-        />
-        <FieldErrorMessages name='username' errors={errors} />
+            render={({ field }) =>
+              <TextField {...field}
+                         label='ユーザー名'
+                         required
+                         InputLabelProps={{
+                           shrink: true,
+                         }}
+                         error={!!errors.username}
+                         helperText={errors.username?.message || '英数字で4文字以上入力してください'}
+              />}
+          />
+          <FieldErrorMessages name='username' errors={errors} />
 
-        <Box>
-          <Stack>
-            <Controller
-              name='password'
-              control={control}
-              rules={enabledRandomPassword ? { required: false } : { required: '入力してください' }}
-              defaultValue=''
-              render={({ field }) =>
-                <TextField {...field}
-                           label='パスワード'
-                           required={!enabledRandomPassword}
-                           disabled={enabledRandomPassword}
-                           placeholder='********'
-                           type='password'
-                           variant='filled'
-                           InputLabelProps={{
-                             shrink: true,
-                           }}
-                           error={!!errors.password}
-                           helperText={errors.password?.message}
-                />
-              }
-            />
+          <Box>
+            <Stack>
+              <Controller
+                name='password'
+                control={control}
+                rules={enabledRandomPassword ? { required: false } : { required: '入力してください' }}
+                defaultValue=''
+                render={({ field }) =>
+                  <TextField {...field}
+                             label='パスワード'
+                             required={!enabledRandomPassword}
+                             disabled={enabledRandomPassword}
+                             placeholder='********'
+                             type='password'
+                             variant='filled'
+                             InputLabelProps={{
+                               shrink: true,
+                             }}
+                             error={!!errors.password}
+                             helperText={errors.password?.message}
+                  />
+                }
+              />
 
-            <Box mt={errors.password ? 2 : 0}>
-              <FieldErrorMessages name='password' errors={errors} />
-            </Box>
+              <Box mt={errors.password ? 2 : 0}>
+                <FieldErrorMessages name='password' errors={errors} />
+              </Box>
 
-            <FormControlLabel
-              control={
-                <Checkbox name='randomPasswordCheck'
-                          checked={enabledRandomPassword}
-                          onChange={handleChangeRandomPassword}
-                />
-              }
-              label='ランダムなパスワードを使用する'
-            />
-          </Stack>
-        </Box>
-        <Box sx={{ py: 2 }}>
-          <Stack direction='row'
-                 spacing={2}
-            // justifyContent='space-between'
-                 alignItems='center'
-          >
-            <Controller
-              name='firstName'
-              control={control}
-              defaultValue=''
-              render={({ field }) =>
-                <TextField {...field}
-                           label='姓'
-                           InputLabelProps={{
-                             shrink: true,
-                           }}
-                           error={!!errors.firstName}
-                           helperText={errors.firstName?.message}
-                           sx={{ width: '100%' }}
-                />
-              }
-            />
-            <Controller
-              name='lastName'
-              control={control}
-              defaultValue=''
-              render={({ field }) =>
-                <TextField {...field}
-                           label='名'
-                           InputLabelProps={{
-                             shrink: true,
-                           }}
-                           error={!!errors.firstName}
-                           helperText={errors.firstName?.message}
-                           sx={{ width: '100%' }}
-                />
-              }
-            />
-          </Stack>
-        </Box>
-        <FormErrorMessages errors={nonFieldErrors} />
-        <Button
-          type='submit'
-          variant='contained'
-          disabled={loading}
-        >登録</Button>
-      </Stack>
-    </form>
+              <FormControlLabel
+                control={
+                  <Checkbox name='randomPasswordCheck'
+                            checked={enabledRandomPassword}
+                            onChange={handleChangeRandomPassword}
+                  />
+                }
+                label='ランダムなパスワードを使用する'
+              />
+            </Stack>
+          </Box>
+          <Box sx={{ py: 2 }}>
+            <Stack direction='row'
+                   spacing={2}
+              // justifyContent='space-between'
+                   alignItems='center'
+            >
+              <Controller
+                name='firstName'
+                control={control}
+                defaultValue=''
+                render={({ field }) =>
+                  <TextField {...field}
+                             label='姓'
+                             InputLabelProps={{
+                               shrink: true,
+                             }}
+                             error={!!errors.firstName}
+                             helperText={errors.firstName?.message}
+                             sx={{ width: '100%' }}
+                  />
+                }
+              />
+              <Controller
+                name='lastName'
+                control={control}
+                defaultValue=''
+                render={({ field }) =>
+                  <TextField {...field}
+                             label='名'
+                             InputLabelProps={{
+                               shrink: true,
+                             }}
+                             error={!!errors.firstName}
+                             helperText={errors.firstName?.message}
+                             sx={{ width: '100%' }}
+                  />
+                }
+              />
+            </Stack>
+          </Box>
+          <FormErrorMessages errors={nonFieldErrors} />
+          <Button type='submit' variant='contained' disabled={loading}>登録</Button>
+        </Stack>
+      </form>
+
+      {registeredMessage()}
+    </>
   );
 };
 export default CreateForm;
